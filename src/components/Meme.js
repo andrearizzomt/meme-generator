1fd32338c944a1efbebb44cd7ba98258fb5cc2ee
Andrea Rizzo <andrearizzo85@gmail.com>
2022-04-27 18:22:04 +0200

    Add dummy data to  generate random meme from it

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,25 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme() {
+  const [memeImage, setMemeImage] = React.useState(""); //error on first load no image loads due to being empty
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  }
+
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input className="form--input" type="text" placeholder="Top Text" />
         <input className="form--input" type="text" placeholder="Bottom Text" />
-        <button className="form--button">Get a new meme Image!</button>
-      </form>
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme Image!
+        </button>
+      </div>
+      <img className="meme--image" src={memeImage} alt="Meme"></img>
     </main>
   );
 }
